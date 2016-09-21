e02b293a129d71a3572de76db02c0513f5965981
Mike Cunneen <mike@appworkshop.net>
2016-09-21 11:56:04 +0800

    Logic was wrong, and it was very noisy.

--- scripts/afterBuild.js
+++ scripts/afterBuild.js
@@ -10,30 +10,44 @@ module.exports = function(context) {
   var config = new ConfigParser(configXml);
   var projectName = config.name();
 
-  var wwwPath = path.join(context.opts.projectRoot, 'platforms/ios/www');
-  var indexHTMLPath = path.join(wwwPath, projectName,
-      'index.html');
+  var wwwPath = path.join(context.opts.projectRoot, 'platforms/ios/www/application/');
+  var otherWwwPath = path.join(context.opts.projectRoot, 'www/application/index.html');
 
-  console.log(context.opts.plugin.id + ' updating META tag for file ' + indexHTMLPath);
-  var oldMetaPattern = '<meta http-equiv="Content-Security-Policy" [^\n]*$';
+
+  var indexHTMLPath = path.join(wwwPath, 'index.html');
+
+  var oldMetaPattern = '  \<meta http-equiv="Content-Security-Policy" [^\n]*';
 
   var data = fs.readFileSync(indexHTMLPath, {'encoding': 'utf8'});
 
-  var oldMetaRegexp = new RegExp(oldMetaPattern);
+  var oldMetaRegexp = new RegExp(oldMetaPattern, 'i');
   var newmeta = `<meta http-equiv="Content-Security-Policy" content="default-src * data: blob: 'unsafe-inline' 'unsafe-eval' ws: wss:;">`;
 
-  if (!oldMetaRegexp.test(data)) {
+  if (oldMetaRegexp.test(data)) {
     var newdata = data.replace(oldMetaRegexp, newmeta);
+
     fs.writeFileSync(indexHTMLPath, newdata);
+
+    console.log(context.opts.plugin.id + ' updating META tag for file ' + indexHTMLPath);
+
+
+    if (fs.existsSync(otherWwwPath)) {
+       data = fs.readFileSync(otherWwwPath, {'encoding': 'utf8'});
+       newdata = data.replace(oldMetaRegexp, newmeta);
+       fs.writeFileSync(otherWwwPath, newdata);
+       console.log(context.opts.plugin.id + ' updating META tag for file ' + otherWwwPath);
+
+    }
   } else {
     // no meta tag found. So we'll add one.
     // first find the head.
     var headpattern = '<head>';
     var headregexp = new RegExp(headpattern, 'i');
-    if (headregexp.text(data)) {
-        // replace <head> with <head>\n<meta....>
-        var newdata = data.replace(headregexp, `${headpattern}\n${newmeta}`);
-        fs.writeFileSync(indexHTMLPath, newdata);
-    }
+    if (headregexp.test(data)) {
+      // replace <head> with <head>\n<meta....>
+      var newdata = data.replace(headregexp, `${headpattern}\n${newmeta}`);
+      
+      fs.writeFileSync(indexHTMLPath, newdata);
+    } 
   }
 }
